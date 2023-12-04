95c05f0a0f62f17dd4587db9b10856b498f85996
Flavio Esteves <flavioafesteves@gmail.com>
2023-12-04 15:02:16 +0000

    Rust: day1_part2

--- rust_aoc/src/day_1/day_1.rs
+++ rust_aoc/src/day_1/day_1.rs
@@ -1,49 +1,73 @@
 use crate::utils::*;
 
-pub struct Part1 {}
+pub fn retrieve_calibration_values() {
+    let read_document = read_file("day1.txt".to_string());
+    let values = calculate_values(read_document, false);
+    let sum_value = sum_values(values);
 
-impl Part1 {
-    pub fn retrieve_calibration_values() {
-        let read_document = read_file("day1.txt".to_string());
-        let values = Self::calculate_values(read_document);
-        let sum_value = sum_values(values);
-
-        println!("Day 1 Part 1 result: {}", sum_value);
-    }
-
-    fn calculate_values(document: String) -> Vec<i32> {
-        let data_parsed: Vec<String> = split_text_lines(document);
-        let mut calculate_values = Vec::new();
-
-        for line in data_parsed {
-            let mut only_numerics = Vec::new();
-            for c in line.chars() {
-                if c.is_numeric() {
-                    only_numerics.push(c)
-                }
-            }
-
-            let first_value = only_numerics[0];
-            let last_value = only_numerics[only_numerics.len() - 1];
-            let concatenated_string = format!("{}{}", first_value, last_value);
-
-            if let Ok(result) = concatenated_string.parse::<i32>() {
-                calculate_values.push(result);
-            } else {
-                println!("Failed to convert")
-            }
-        }
-        calculate_values
-    }
+    println!("Day 1 Part 1 result: {}", sum_value);
 }
 
+pub fn retrieve_calibration_values_part2() {
+    let read_document = read_file("day1.txt".to_string());
+    let values = calculate_values(read_document, true);
+    let sum_value = sum_values(values);
+
+    println!("Day 1 Part 2 result: {}", sum_value);
+}
+
+fn calculate_values(document: String, is_part2: bool) -> Vec<i32> {
+    let data_parsed: Vec<String> = split_text_lines(document);
+    let mut calculate_values = Vec::new();
+
+    for mut line in data_parsed {
+        let mut only_numerics = Vec::new();
+
+        if is_part2 {
+            line = replace_string_values_with_numbers(line);
+        }
+
+        for c in line.chars() {
+            if c.is_numeric() {
+                only_numerics.push(c)
+            }
+        }
+
+        let first_value = only_numerics[0];
+        let last_value = only_numerics[only_numerics.len() - 1];
+        let concatenated_string = format!("{}{}", first_value, last_value);
+
+        if let Ok(result) = concatenated_string.parse::<i32>() {
+            calculate_values.push(result);
+        } else {
+            println!("Failed to convert")
+        }
+    }
+    calculate_values
+}
 fn sum_values(values: Vec<i32>) -> i32 {
     let sum = values.iter().sum();
     sum
 }
 
+fn replace_string_values_with_numbers(line: String) -> String {
+    let line = line.replace("one", "one1one");
+    let line = line.replace("two", "two2two");
+    let line = line.replace("three", "three3three");
+    let line = line.replace("four", "four4four");
+    let line = line.replace("five", "five5five");
+    let line = line.replace("six", "six6six");
+    let line = line.replace("seven", "seven7seven");
+    let line = line.replace("eight", "eight8eight");
+    let line = line.replace("nine", "nine9nine");
+
+    line
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::day_1::day_1::{calculate_values, sum_values};
+
     #[test]
     fn test_sample() {
         let test_example = "two1nine
@@ -53,6 +77,10 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen";
-        assert_eq!("hello", test_example);
+
+        let values = calculate_values(test_example.to_string(), true);
+        let sum_value = sum_values(values);
+
+        assert_eq!(281, sum_value);
     }
 }
--- rust_aoc/src/main.rs
+++ rust_aoc/src/main.rs
@@ -1,5 +1,6 @@
-use rust_aoc::day_1::Part1;
+use rust_aoc::day_1::{retrieve_calibration_values, retrieve_calibration_values_part2};
 
 fn main() {
-    let _day1 = Part1::retrieve_calibration_values();
+    let _day1_part1 = retrieve_calibration_values();
+    let _day1_part2 = retrieve_calibration_values_part2();
 }
